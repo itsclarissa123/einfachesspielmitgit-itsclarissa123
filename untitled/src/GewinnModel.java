22b8b34b2efc02545698a93bdb69d199e3cdb8ba
itsClarissa123 <cczipin@student.tgm.ac.at>
2024-09-15 17:28:24 +0200

    Adding new the logic behind the game

---
+++ untitled/src/GewinnModel.java
@@ -0,0 +1,59 @@
+import java.util.Random;
+/**
+ * Ganze Logik hinter dem Spiel
+ * @author Clarissa Czipin
+ * @version 2024-09-15
+ */
+public class MModel {
+    int spielerZahl;
+    int computerZahl;
+    int gesamtPunkte;
+    int rundenErgebnis;
+
+    public GewinnModel(0){
+        berechneComputerZahl();
+        this.gesamtPunkte = 30;
+        this.rundenErgebnis = 0;
+    }
+    public GewinnModel(){
+        berechneComputerZahl();
+    }
+    public int getGesamtPunkte(){
+        return this.gesamtPunkte;
+    }
+    public int getComputerZahl(){
+        return this.computerZahl;
+    }
+    public int getRundenErgebnis(){
+        return this.rundenErgebnis;
+    }
+    public void berechneComputerZahl(){
+        Random rn = new Random();
+        this.computerZahl = rn.nextInt(9)+1;
+    }
+    public void berechneRunde(int spielerZahl){
+        int genau = 20;
+        int fast = 5;
+        int nicht = -10;
+        if (zahlIsValid(spielerZahl)) {
+            if (this.computerZahl == this.spielerZahl) {
+                this.rundenErgebnis = genau;
+                this.gesamtPunkte += genau;
+            } else if (this.computerZahl - 1 == this.spielerZahl || this.computerZahl + 1 == this.spielerZahl) {
+                this.rundenErgebnis = fast;
+                this.gesamtPunkte += fast;
+            } else {
+                this.rundenErgebnis = nicht;
+                this.gesamtPunkte += nicht;
+            }
+        }else{
+            return 11;
+        }
+    }
+    public boolean zahlIsValid(int z){
+        if(z > 9 || z < 1){
+            return false;
+        }
+        return true;
+    }
+}
